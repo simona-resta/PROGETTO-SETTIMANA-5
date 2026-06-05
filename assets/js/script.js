4bbd28b449b479dc73319658865a8323275676a7
Simona Resta <lavoro.resta@gmail.com>
2026-06-06 01:27:49 +0200

    header sticky

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,13 @@
 // Aggiungi qui il codice JavaScript se serve (es. modale, scroll handler).
 // Per la Versione Base spesso non serve niente: tutto si fa in CSS.
 
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('site-header');
+    
+    // Se scrolliamo più di 100px
+    if (window.scrollY > 100) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
